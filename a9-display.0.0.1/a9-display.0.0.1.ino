e0c137494dd582a9b8b87d2b6bc16bbfe9252fdc
Michael C. Libby <m@mlibby.com>
2015-11-15 09:42:38 -0600

    woof! woof! on display

---
+++ a9-display.0.0.1/a9-display.0.0.1.ino
@@ -0,0 +1,41 @@
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+
+// Software SPI (slower updates, more flexible pin options):
+// pin 7 - Serial clock out (SCLK)
+// pin 6 - Serial data out (DIN)
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
+
+// Hardware SPI (faster, but must use certain hardware pins):
+// SCK is LCD serial clock (SCLK) - this is pin 13 on Arduino Uno
+// MOSI is LCD DIN - this is pin 11 on an Arduino Uno
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+// Adafruit_PCD8544 display = Adafruit_PCD8544(5, 4, 3);
+// Note with hardware SPI MISO and SS pins aren't used but will still be read
+// and written to during SPI transfer.  Be careful sharing these pins!
+
+
+void setup()   {
+  Serial.begin(9600);
+  display.begin();
+  display.setContrast(30);
+  
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.setTextSize(2);
+  display.setTextColor(BLACK);
+  display.print("w00f!");
+  display.setCursor(12,18);
+  display.print("w00f!");
+  display.display();
+}
+
+void loop() {
+}
+
